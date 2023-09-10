6238ef2b5140aec4d2b7456394dde40f279da421
Dan Morris <dan@morris2morris.com>
2023-09-09 21:23:23 -0400

    Update ISolver.cs

--- Sudoku.Engine/Solvers/ISolver.cs
+++ Sudoku.Engine/Solvers/ISolver.cs
@@ -4,11 +4,8 @@ namespace Sudoku.Engine.Solvers
 {
   public interface ISolver
   {
-    //public IGameBoard Solve(IGameBoard gameBoard);
-    //public void SetGameBoard(IGameBoard gameBoard);
     public IGameBoard GetGameBoard();
     public int Solve(IGameBoard gameBoard);
     public int GetExecutionOrder();
-
   }
 }
